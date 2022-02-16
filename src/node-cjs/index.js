182c4c2880beb9fc44b95b83cbb9da51af41155c
06000208 <52764066+06000208@users.noreply.github.com>
2022-02-15 22:13:58 -0600

    Update index.js

---
+++ src/node-cjs/index.js
@@ -0,0 +1,5 @@
+exports.Block = require("./structures/Block.js");
+exports.BlockModule = require("./structures/BlockModule.js");
+exports.Box = require("./structures/Box.js");
+exports.BoxModule = require("./structures/BoxModule.js");
+exports.Construct = require("./structures/Construct.js");
