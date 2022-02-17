c80e9f36ed97e59d44c0716823c13684bb931229
06000208 <52764066+06000208@users.noreply.github.com>
2022-02-17 02:50:19 -0600

    Update index files

--- src/index.cjs.js
+++ src/index.cjs.js
@@ -3,3 +3,4 @@ export * from "./structures/BlockModule.js";
 export * from "./structures/Box.js";
 export * from "./structures/BoxModule.js";
 export * from "./structures/Construct.js";
+// export * as RequireLoader from "./node/cjs/RequireLoader.js";
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-throw Error("Unfinished");
+export * from "./structures/Block.js";
+export * from "./structures/BlockModule.js";
+export * from "./structures/Box.js";
+export * from "./structures/BoxModule.js";
+export * from "./structures/Construct.js";
