c0ed1b76e3fd6248a90f3539f540cbf91cd77454
06000208 <52764066+06000208@users.noreply.github.com>
2022-03-13 00:25:51 -0600

    Create ModuleConstruct.js

---
+++ src/structures/ModuleConstruct.js
@@ -0,0 +1,44 @@
+import { Construct } from "./Construct.js";
+import { pushArrayInMap, filterArrayInMap } from "../util/maps.js";
+
+/**
+ * @abstract
+ */
+class ModuleConstruct extends Construct {
+    /**
+     * @param {Map} [cache] A javascript Map, or something which implements the
+     * Map API such as \@discordjs/collection
+     * @param {Map<?string, [string]>} [idsByModuleSpecifier] A javascript Map,
+     * or something which implements the Map API such as \@discordjs/collection
+     */
+    constructor(cache = new Map(), idsByModuleSpecifier = new Map()) {
+        super(cache);
+
+        /**
+         * @type {Map<?string, [string]>}
+         */
+        this.idsByModuleSpecifier = idsByModuleSpecifier;
+    }
+
+    /**
+     * Loads a block, intended to be extended (using the super keyword)
+     * @param {{id: *, moduleSpecifier: ?string}} block
+     * @returns {boolean} true for success, false for failure
+     */
+    load(block) {
+        pushArrayInMap(this.idsByModuleSpecifier, block.moduleSpecifier, block.id);
+        return super.load(block);
+    }
+
+    /**
+     * Unloads a block, intended to be extended (using the super keyword)
+     * @param {{id: *, moduleSpecifier: ?string}} block
+     * @returns {boolean} true for success, false for failure
+     */
+    unload(block) {
+        filterArrayInMap(this.idsByModuleSpecifier, block.moduleSpecifier, block.id);
+        return super.unload(block);
+    }
+}
+
+export { ModuleConstruct };
