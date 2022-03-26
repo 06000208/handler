57c6f13bafe82571a682b466321a0db5a406dc90
06000208 <52764066+06000208@users.noreply.github.com>
2022-03-26 11:22:57 -0500

    Update index files

--- src/index.cjs.js
+++ src/index.cjs.js
@@ -1,9 +1,2 @@
-export * from "./structures/Block.js";
-export * from "./structures/BlockModule.js";
-export * from "./structures/Box.js";
-export * from "./structures/BoxModule.js";
-export * from "./structures/Construct.js";
-export * from "./structures/Sorter.js";
-export * from "./util/id.js";
-export * from "./util/misc.js";
+export * from "./index.js";
 // export * as RequireLoader from "./node/cjs/RequireLoader.js";
