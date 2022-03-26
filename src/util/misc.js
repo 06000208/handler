1eaa09b1fbf20ea7291e2af46f9bf04af2007a10
06000208 <52764066+06000208@users.noreply.github.com>
2022-03-26 10:12:52 -0500

    Update misc.js

--- src/util/misc.js
+++ src/util/misc.js
@@ -4,10 +4,17 @@
  */
 
 /**
+ * Checks whether or not a value is a function
+ * @param {*} value The value being checked
+ * @returns {boolean} True if value is a function, otherwise false
+ */
+const isFunction = (value) => value && typeof value == "function";
+
+/**
  * Checks whether or not a value is a boolean
- * @param {*} value - The value being checked
- * @returns {boolean} - True if value is a boolean, otherwise false
+ * @param {*} value The value being checked
+ * @returns {boolean} True if value is a boolean, otherwise false
  */
 const isBoolean = (value) => value === true || value === false;
 
-export { isBoolean };
+export { isFunction, isBoolean };
