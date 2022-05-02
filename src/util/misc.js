a221fc5c9d7c983d45e92588c5fd8f202aa7b60c
06000208 <52764066+06000208@users.noreply.github.com>
2022-05-01 23:25:19 -0500

    Remove isBoolean

--- src/util/misc.js
+++ src/util/misc.js
@@ -10,11 +10,4 @@
  */
 const isFunction = (value) => value && typeof value == "function";
 
-/**
- * Checks whether or not a value is a boolean
- * @param {*} value The value being checked
- * @returns {boolean} True if value is a boolean, otherwise false
- */
-const isBoolean = (value) => value === true || value === false;
-
-export { isFunction, isBoolean };
+export { isFunction };
