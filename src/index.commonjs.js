daf3a2d29548f75d8bd6bfbc9c4e00b4703b6d05
amber <a06000208@protonmail.com>
2022-10-27 01:30:47 -0500

    experiment with cjs/esm exporting

--- src/index.commonjs.js
+++ src/index.commonjs.js
@@ -1,14 +1,2 @@
 export * from "./node.js";
-import * as node from "./node.js";
-import * as RequireLoader from "./node/cjs/RequireLoader.cjs";
-export { RequireLoader };
-export default {
-    ...node,
-    ...RequireLoader,
-};
-// import { RequireLoader } from "./node/cjs/RequireLoader.cjs";
-// export { RequireLoader };
-// export default {
-//     ...node,
-//     "RequireLoader": RequireLoader,
-// };
+export { RequireLoader } from "./node/cjs/RequireLoader.cjs";
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 export * from "./node.js";
+export * from "./node/ImportLoader.js";
+export * from "./node/ImportQueryLoader.js";
+
 import * as node from "./node.js";
-export default { ...node };
+import { ImportLoader } from "./node/ImportLoader.js";
+import { ImportQueryLoader } from "./node/ImportQueryLoader.js";
+
+export default {
+    ...node,
+    ImportLoader,
+    ImportQueryLoader,
+};
