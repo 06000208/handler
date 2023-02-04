653714e30417ff068c94aad898fbf55ff01f14b7
amber <a06000208@protonmail.com>
2023-02-04 02:34:54 -0600

    Export data wrapper, data stores, and updated isURL function

--- src/modules.js
+++ src/modules.js
@@ -2,4 +2,7 @@ export * from "./any/Construct.js";
 export * from "./any/Block.js";
 export * from "./any/BlockModule.js";
 export * from "./any/Sorter.js";
+export * from "./any/DataStore.js";
+export * from "./any/AsyncDataStore.js";
 export * from "./any/util/id.js";
+export * from "./any/util/isURL.js";
