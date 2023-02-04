48e197b261ad15c4d582a3dbd893d3d1cd6f14da
amber <a06000208@protonmail.com>
2023-02-04 01:49:12 -0600

    update deps

--- src/any/util/isURL.js
+++ src/any/util/isURL.js
@@ -1,12 +1,13 @@
 import { URL } from "node:url";
 
 /**
- * Uses the URL class compliant with the WHATWG URL Standard to check if a
- * string is considered a URL.
+ * Uses some checks and the URL class (compliant with the WHATWG URL Standard) to check if a string is considered a URL.
  * @param {*} input
  * @returns {boolean}
  */
 export const isURL = function(input) {
+    if (!input) return false;
+    if (Array.isArray(input)) return false;
     try {
         new URL(input);
         return true;
