a4262c9c918b5c3ce41834a4598c7a52b3f670ee
amber <a06000208@protonmail.com>
2023-02-04 02:22:40 -0600

    Box is unnecessary

--- src/modules.js
+++ src/modules.js
@@ -2,7 +2,5 @@ export * from "./any/BaseConstruct.js";
 export * from "./any/Construct.js";
 export * from "./any/Block.js";
 export * from "./any/BlockModule.js";
-export * from "./any/Box.js";
-export * from "./any/BoxModule.js";
 export * from "./any/Sorter.js";
 export * from "./any/util/id.js";
