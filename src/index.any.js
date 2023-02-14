23f1638c5fcc94d4b65694f1a4f89b738cb2da82
amber <a06000208@protonmail.com>
2023-02-13 23:23:43 -0600

    Data wrappers are provided by a different package now
    See the 06000208/data-wrappers repository if you want these

--- src/index.any.js
+++ src/index.any.js
@@ -3,7 +3,4 @@ export * from "./any/Construct.js";
 export * from "./any/Block.js";
 export * from "./any/BlockModule.js";
 export * from "./any/Sorter.js";
-export * from "./any/FlatDataStore.js";
-export * from "./any/AsyncFlatDataStore.js";
-export * from "./any/FlatDataWrapper.js";
 export * from "./any/util/id.js";
